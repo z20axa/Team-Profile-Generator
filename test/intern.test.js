ecc968955f41923d5bd300eaa1ce699f93677556
z20axa <alejandrospan@hotmail.com>
2022-12-10 17:43:00 -0500

    Adds test code for intern file

---
+++ test/intern.test.js
@@ -0,0 +1,12 @@
+const Intern = require("../lib/intern");
+
+describe("Intern", () => {
+    describe("Initialization", () => {
+      it("should have an intern constructor", () => {
+        const obj = new Intern();
+  
+        expect(obj instanceof Intern).toEqual(true);
+  
+      });
+    })
+  })
