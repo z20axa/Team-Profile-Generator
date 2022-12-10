3798d053639b2790b5d3b967fd21e131d2ba240e
z20axa <alejandrospan@hotmail.com>
2022-12-10 17:56:56 -0500

    Adds revisions to employee test file

--- test/employee.test.js
+++ test/employee.test.js
@@ -7,7 +7,22 @@ describe("Employee", () => {
         const obj = new Employee();
   
         expect(obj instanceof Employee).toEqual(true);
-  
       });
+
+    describe("Employee Name", () => {
+    it("should have a employee name", () => {
+        const name = "Bob";
+        const e = new Employee(name);
+        expect (e.name).tobe(name);
+    });
+
+    // describe("Employee ID", () => {
+    //     it("should have a employee ID", () => {
+    //         const id = "1";
+    //         const eId = new Employee(name);
+    //         expect (e.name).tobe(name);
+    // });
+
+
     })
   })
