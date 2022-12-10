5a82a98815194d5dfe565f81dff0c73b5b1553d6
z20axa <alejandrospan@hotmail.com>
2022-12-10 17:41:41 -0500

    Adds test code for manager file

---
+++ test/manager.test.js
@@ -0,0 +1,12 @@
+const Manager = require("../lib/manager");
+
+describe("Manager", () => {
+    describe("Initialization", () => {
+      it("should have a manager constructor", () => {
+        const obj = new Manager();
+  
+        expect(obj instanceof Manager).toEqual(true);
+  
+      });
+    })
+  })
