b52384dcdefbdb2e8df1d62ea5024ddd421b38a3
z20axa <alejandrospan@hotmail.com>
2023-01-31 18:04:58 -0500

    Adds more tests to the manager extended class

--- test/manager.test.js
+++ test/manager.test.js
@@ -1,32 +1,47 @@
+// variable declarations
 const Manager = require("../lib/manager");
-
 const test = new Manager("Jared", 1, "jared@fakeemail.com", 1);
 
 describe("Manager", () => {
-    describe("Initialization", () => {
-      it("should have a manager constructor", () => {
-        const obj = new Manager();
-        expect(obj instanceof Manager).toEqual(true);
-      });
-      it("should have a manager office number", () => {
-        const managerOfficeNo = 1;
-        expect(test.officeNumber).toBe(managerOfficeNo);
-      });
-    })
+  describe("Initialization", () => {
+    // positive test
+    it("should have a manager constructor", () => {
+      // arrange
+      const obj = new Manager();
 
-    // describe("getOfficeNumber", () => {
-    //   it("should return a manager office number", () => {
-    //     // // SEET
-    //     // const obj = new Employee();
-    //     // expect(obj instanceof Employee).toEqual(true);
-    //   });
-    // })
-        
-    // describe("getRole", () => {
-    //   it("should return a manager role", () => {
-    //     // // SEET
-    //     // const obj = new Employee();
-    //     // expect(obj instanceof Employee).toEqual(true);
-    //   });
-    // })
+      // assert
+      expect(obj instanceof Manager).toEqual(true);
+    });
+
+    // positive test
+    it("should have a manager office number", () => {
+      // arrange
+      const managerOfficeNo = 1;
+
+      // assert
+      expect(test.officeNumber).toBe(managerOfficeNo);
+    });
+  })
+
+  describe("getOfficeNumber", () => {
+    // positive test
+    it("should return a manager office number", () => {
+      // arrange
+      const managerOfficeNo = 1;
+
+      // assert
+      expect(test.getOfficeNumber()).toBe(managerOfficeNo);
+    });
+  })
+
+  describe("getRole", () => {
+    // positive test
+    it("should return a manager role", () => {
+      // arrange 
+      const managerRole = "Manager";
+
+      // assert
+      expect(test.getRole()).toBe(managerRole);
+    });
+  })
 })
